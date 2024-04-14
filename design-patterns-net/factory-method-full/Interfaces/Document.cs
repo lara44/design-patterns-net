3c26d68873fae63053b6858c3d9c87e30089cc98
luis.rojas <luis.rojas1@softwareone.com>
2024-04-14 18:57:53 -0500

    Refactor Factory Method

---
+++ design-patterns-net/factory-method-full/Interfaces/Document.cs
@@ -0,0 +1,9 @@
+﻿namespace factory_method_full.Interfaces
+{
+    public interface Document
+    {
+        public string getConfig();
+        public string getPayload();
+        public string printDocument();
+    }
+}
