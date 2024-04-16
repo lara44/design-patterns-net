7ae3943d4ba1d129ae2aa70ebd2af3cd4bb35939
luis.rojas <luis.rojas1@softwareone.com>
2024-04-16 11:22:45 -0500

    [ADD] Adapter Pattern

---
+++ design-patterns-net/pattern-adapter/Services/NequiMethod.cs
@@ -0,0 +1,10 @@
+﻿namespace pattern_adapter.Services
+{
+    public class NequiMethod
+    {
+        public void PayWithNequi(decimal value)
+        {
+            Console.WriteLine($"Pagando {value:C} en Nequi");
+        }
+    }
+}
